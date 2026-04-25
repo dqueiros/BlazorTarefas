5aa929fc86d2da1cfad038d083119440dcc4e1bc
dqueiros <diegoqueiros@hotmail.com>
2026-04-25 17:16:35 -0300

    criando o componente Tarefas

---
+++ BlazorTarefas/BlazorTarefas/Data/Tarefa.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorTarefas.Data
+{
+    public class Tarefa
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string? Descricao { get; set; }
+        public bool Concluida { get; set; }
+        public DateTime DataCriacao { get; set; }
+    }
+}
